7d956d99254c26f2774f994fa4c9c858cc808308
Varandeep07 <varandeepsahota@gmail.com>
2023-05-29 10:26:44 +0530

    done

--- script.js
+++ script.js
@@ -3,11 +3,6 @@ const form = document.querySelector("form");
 const search = document.querySelector("#search");
 const weather = document.querySelector("#weather");
 
-// const API = `https://api.openweathermap.org/data/2.5/weather?
-//      q=${city}&appid=${API_KEY}&units=metric`;
-
-// const IMG_URL = `https: //openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-
 const getWeather = async (city) => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
     const response = await fetch(url);
